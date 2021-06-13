e17e926f71ce832000ea0868068dca0f98841299
BilalRehman08 <bilalrehman080808@gmail.com>
2021-06-13 03:42:15 -0700

    Home Page Layout using listView(direction: horizontal) and GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,101 +12,5 @@ class MyApp extends StatelessWidget {
         body: Home(),
       ),
     );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<dynamic> lst = [1, 2, 3, 4, 5, 6, 7, 8];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Container(
-              color: Colors.red,
-              height: 210,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                  Container(
-                    color: Colors.amber,
-                    width: 200,
-                    height: 200,
-                    margin: EdgeInsets.all(10),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            GridView.count(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              crossAxisCount: 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-              children: List.generate(lst.length, (index) {
-                return Container(
-                  color: Colors.brown,
-                  height: 100,
-                  width: 100,
-                );
-              }),
-            )
-          ],
-        ),
-      ),
-    ));
   }
 }
